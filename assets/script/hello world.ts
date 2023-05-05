e4bf86b3d2d2f86d49cd10c9bada500758cb4298
meng <1363992060@qq.com>
2023-05-06 00:32:42 +0800

    初始化

---
+++ assets/script/hello world.ts
@@ -0,0 +1,14 @@
+import { _decorator, Component, Node } from 'cc';
+const { ccclass, property } = _decorator;
+
+@ccclass('hello_world')
+export class hello_world extends Component {
+    start() {
+
+    }
+
+    update(deltaTime: number) {
+        
+    }
+}
+
